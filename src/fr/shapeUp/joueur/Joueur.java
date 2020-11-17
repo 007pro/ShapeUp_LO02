cc623a575d03d99e683ede80b8442230227b7a09
AdrienWarnet <awarnet1109@gmail.com>
2020-11-17 15:55:09 +0100

    test push joueur

--- src/fr/shapeUp/joueur/Joueur.java
+++ src/fr/shapeUp/joueur/Joueur.java
@@ -9,4 +9,41 @@ package fr.shapeUp.joueur;
  */
 public abstract class Joueur {
 
+	/**
+	 * score, vas représenter le score du joueur
+	 */
+	private int score = 0;
+	
+	//private Carte carteVictoire = new carte();
+	private String carteVictoire ; // en attandant la création de la classe carte je fais un string pour simuler
+	private int carte; //la carte qu'il à dans la main et qu'il vas jouer simuler en int pour l'instant
+	
+	public Joueur() {
+		this.score = 0;
+		this.carteVictoire = "carte victoire aléatoire"; 
+		this.carte = 0;
+		
+	}
+	
+	public abstract void jouerTour();
+	
+	public void poserCarte (String carte, int position) {
+		
+	}
+	
+	public void deplacerCarte(String carte, int newPosition) {
+		
+	}
+	
+	public void piocher() {
+		//et avec un enum de cartes on pourra dire qu'il utilse à pioché cette carte 
+		this.carte = (int) (Math.random() * ( 14 - 0 ));
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xesh");
+		
+	}
 }
