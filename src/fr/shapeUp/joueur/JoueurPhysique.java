9d6eed4a64a34f1f592e9ea5f934c7420bc563f1
007pro <yopbbb@gmail.com>
2020-11-17 15:49:37 +0100

    Push test

--- src/fr/shapeUp/joueur/JoueurPhysique.java
+++ src/fr/shapeUp/joueur/JoueurPhysique.java
@@ -1,7 +1,7 @@
 package fr.shapeUp.joueur;
 
 /**
- * La classe joueur physique vas permettre a l'utilisateur de jouer une partie 
+ * La classe joueur physique va permettre a l'utilisateur de jouer une partie 
  * 
  * @author Adrien Warnet, Vincent Diop
  * @version 1.0
