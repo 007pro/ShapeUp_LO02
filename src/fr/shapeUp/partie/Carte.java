3b0c03596832ab93535037f292a89217a7a78c71
007pro <yopbbb@gmail.com>
2020-11-17 18:12:41 +0100

    Ajout carte complète (avec creation de carte random)

--- src/fr/shapeUp/partie/Carte.java
+++ src/fr/shapeUp/partie/Carte.java
@@ -7,16 +7,48 @@ import java.util.Random;
 //Ceci est une carte !
 public class Carte {
 	
-	public enum LesCartes {
+	private boolean remplie;
+	private formeCarte forme;
+	private couleurCarte couleur;
+	
+	public enum formeCarte {
 		Carré,
 		Rond,
 		Triangle
 
 	}
-
-	public static LesCartes randomCarte() {
-	    int pick = new Random().nextInt(LesCartes.values().length);
-	    return LesCartes.values()[pick];
+	
+	public enum couleurCarte {
+		Rouge,
+		Vert,
+		Bleu
+	}
+	
+	Carte(formeCarte forme, couleurCarte couleur, boolean remplie){
+		this.forme = forme;
+		this.couleur = couleur;
+		this.remplie = remplie;	
+	}
+	
+	Carte(){
+		
 	}
 
+	public Carte randomCarte() {
+		int pick = new Random().nextInt(couleurCarte.values().length);
+		int pick2 = new Random().nextInt(formeCarte.values().length);
+	    Carte carteRandom = new Carte(formeCarte.values()[pick2],couleurCarte.values()[pick],new Random().nextBoolean());
+	    return carteRandom;
+	}
+
+//	public static void main (String[] args){
+//		Carte carte1 = new Carte().randomCarte();
+//		System.out.print("Remplie :" + carte1.remplie);
+//		System.out.print(" Couleur : " + carte1.couleur);
+//		System.out.println(" Forme : " + carte1.forme);
+//		Carte carte2 = new Carte().randomCarte();
+//		System.out.print("Remplie :" + carte2.remplie);
+//		System.out.print(" Couleur : " + carte2.couleur);
+//		System.out.println(" Forme : " + carte2.forme);
+//	}
 }
