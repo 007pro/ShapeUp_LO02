252cf1a17845df88c28ce49154ef16238e9d2873
AdrienWarnet <awarnet1109@gmail.com>
2020-11-21 10:31:07 +0100

    Création construteur et methode jouerTour

--- src/fr/shapeUp/joueur/JoueurVirutel.java
+++ src/fr/shapeUp/joueur/JoueurVirutel.java
@@ -1,10 +1,29 @@
 package fr.shapeUp.joueur;
+
+import fr.shapeUp.partie.Partie;
+
 /**
- * La classe joueurVirtuel vas permettre à l'ordinateur de jouer contre un joueurPhysique
+ * La classe joueurVirtuel vas permettre à l'ordinateur de jouer contre un
+ * joueurPhysique
  * 
  * @author Adrien Warnet, Vincent Diop
  * @version 1.0
  */
-public class JoueurVirutel {
+public class JoueurVirutel extends Joueur {
+	
+	boolean tourAutreJoueurFini ;
+
+	public JoueurVirutel(Partie partie, int numJoueur) {
+
+		super(partie, numJoueur);
+
+	}
+
+	@Override
+	public void jouerTour() {
+		
+		// TODO Auto-generated method stub
+
+	}
 
 }
