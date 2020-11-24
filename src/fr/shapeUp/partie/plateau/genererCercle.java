fedfbeee3058cbee2fae183535bb45facfc2d874
007pro <yopbbb@gmail.com>
2020-11-24 12:48:34 +0100

    generation du cercle

--- src/fr/shapeUp/partie/plateau/genererCercle.java
+++ src/fr/shapeUp/partie/plateau/genererCercle.java
@@ -1,10 +1,25 @@
 package fr.shapeUp.partie.plateau;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class genererCercle implements genererClesStrategy {
 	@Override
 	public ArrayList<String> generer(){
-		return null;
+		ArrayList<String> cles = new ArrayList<String>();
+		Scanner saisiUser = new Scanner(System.in);
+		System.out.println("Veuillez indiquer le rayon du cercle (multiples de 0,5 acceptés)");
+		double rayon = saisiUser.nextDouble();
+		for(int i=0;i<rayon*2;i++) {
+	        for(int j=0;j<rayon*2;j++){
+	        	double x = (2*(double)i + 1)/2;
+	        	double y = (2*(double)j + 1)/2;
+	            double d = Math.sqrt((x-rayon)*(x-rayon)+(y-rayon)*(y-rayon));
+	                if(d < rayon) {
+	                	cles.add((char)(j + 65) + Integer.toString(i));
+	                }           
+	        }
+		}
+		return cles;
 	}
 }
