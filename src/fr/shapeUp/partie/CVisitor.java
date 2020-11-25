c970f18e5f33dd4d70560b9cce4123ae436c3a46
AdrienWarnet <awarnet1109@gmail.com>
2020-11-25 19:41:35 +0100

    Javadoc comment

--- src/fr/shapeUp/partie/CVisitor.java
+++ src/fr/shapeUp/partie/CVisitor.java
@@ -2,6 +2,11 @@ package fr.shapeUp.partie;
 
 import fr.shapeUp.partie.plateau.Plateau;
 
+/**
+ * interface CVisitor
+ * @author Adrien Warnet, Vincent Diop
+ *
+ */
 public interface CVisitor {
 	void visitPlateau(Plateau plateau);
 }
--- src/fr/shapeUp/partie/VComptage.java
+++ src/fr/shapeUp/partie/VComptage.java
@@ -1,5 +1,10 @@
 package fr.shapeUp.partie;
 
+/**
+ * interface VComptage
+ * @author Adrien Warnet, Vincent Diop
+ *
+ */
 public interface VComptage {
 	void accept(CVisitor visitor);
 }
