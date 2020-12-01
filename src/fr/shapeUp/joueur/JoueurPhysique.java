20f0474caabaed2da4b7e98538bdc3fead5fd507
AdrienWarnet <awarnet1109@gmail.com>
2020-12-01 20:55:10 +0100

    ajustement de la méthode

--- src/fr/shapeUp/joueur/JoueurPhysique.java
+++ src/fr/shapeUp/joueur/JoueurPhysique.java
@@ -48,7 +48,7 @@ public class JoueurPhysique extends Joueur {
 	public boolean poserCarteOfMain(String position,int numCarte) {
 		//TODO transformer la main courante pour que la carte victoire ne soit pas dedans (je le modifi tout a l'heure)
 		boolean cartePlacé = partie.plateau.placerCarte(position,mainCourante[numCarte -1]);//-1 car le joueur va dire carte 1,2 ou 3 et l'index commence en 0
-		mainCourante[numCarte -1]= null;
+		mainCourante[numCarte -1] = null;
 		if (cartePlacé == true) {
 			System.out.println("Vous avez posé votre carte en " + position);
 			return true;
@@ -93,7 +93,26 @@ public class JoueurPhysique extends Joueur {
 	public void jouerTourAvancé() {
 		super.piocherMainde3();
 		System.out.print("\n");
-		
+		do { 
+			System.out.println("Quelle carte souhaite-tu poser : (1,2 ou 3)");
+			int numCarte= saisiUseur.nextInt();
+			System.out.println("Poser la carte à quelle position? ");
+			String position = saisiUseur.nextLine();
+			setPosition(position);
+			pose = poserCarteOfMain(position, numCarte);
+		} while (pose == false);
+		System.out.print("\n");
+		System.out.println("Déplacer une carte ? 1=oui/2=non ");
+
+		int ouiOuNon = saisiUseur.nextInt();
+		saisiUseur.nextLine();
+		if (ouiOuNon == 1) {
+			super.deplacerCarte();
+			super.finTour();
+
+		} else {
+			super.finTour();
+		}
 		
 	}
 
