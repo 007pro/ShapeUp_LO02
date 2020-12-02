16c1d82c77751c0608b7cc258aeb63232db9a832
AdrienWarnet <awarnet1109@gmail.com>
2020-12-02 18:26:41 +0100

    création de la classe windiwInit mais problème des import

---
+++ src/fr/shapeUp/graphique/WindowInit.java
@@ -0,0 +1,17 @@
+package fr.shapeUp.graphique;
+
+/*import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class WindowInit {
+
+	JFrame fenetre = new JFrame();
+}*/
